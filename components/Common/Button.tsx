4f6d33995ced00de3019b2cb45dc5196bf999613
Ahnaf Alfariza <ahnafalfariza@outlook.com>
2021-11-05 18:06:57 +0700

    add: button component

---
+++ components/Common/Button.tsx
@@ -0,0 +1,70 @@
+import React from 'react'
+
+interface ButtonProps {
+	children: React.ReactNode
+	onClick: React.MouseEventHandler<HTMLButtonElement>
+	isDisabled?: boolean
+	isFullWidth?: boolean
+	size?: 'sm' | 'md' | 'lg'
+	color?: 'blue' | 'red' | 'green'
+	className?: string
+	style?: React.CSSProperties
+}
+
+const Button = ({
+	isDisabled,
+	isFullWidth,
+	size = 'md',
+	color = 'blue',
+	children,
+	className,
+	style,
+	onClick,
+}: ButtonProps) => {
+	const buttonBaseStyle =
+		'inline-block text-center relative whitespace-nowrap font-medium hover:bg-opacity-80'
+	const buttonTransition = 'transition duration-150 ease-in-out'
+	const buttonDisabledStyle = isDisabled
+		? 'cursor-not-allowed opacity-60 saturate-50 hover:bg-opacity-100'
+		: ''
+	const buttonWideStyle = isFullWidth ? 'w-full block' : ''
+
+	const getColorStyle = (): string => {
+		switch (color) {
+			case 'blue':
+				return 'bg-blueButton text-white'
+			case 'red':
+				return 'bg-redButton text-white'
+			case 'green':
+				return 'bg-greenButton text-white'
+			default:
+				return ''
+		}
+	}
+
+	const getSizeStyle = (): string => {
+		switch (size) {
+			case 'lg':
+				return 'py-3 px-20 text-base rounded-xl'
+			case 'md':
+				return 'py-2 px-8 text-sm rounded-lg'
+			case 'sm':
+				return 'py-1 px-4 text-xs rounded-md'
+			default:
+				return ''
+		}
+	}
+
+	return (
+		<button
+			style={style}
+			disabled={isDisabled}
+			className={`${buttonBaseStyle} ${buttonTransition} ${getColorStyle()} ${getSizeStyle()} ${buttonDisabledStyle} ${buttonWideStyle} ${className}`}
+			onClick={onClick}
+		>
+			{children}
+		</button>
+	)
+}
+
+export default Button
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,19 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-    colors: {
-      parasGrey: "#171D2D",
-      blueButton: "#247DFE",
-      redButton: "#EB5757",
-      greenButton: "#21A79F",
-      borderGray: "#272E3E"
-    }
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+	purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			colors: {
+				parasGrey: '#171D2D',
+				blueButton: '#247DFE',
+				redButton: '#EB5757',
+				greenButton: '#21A79F',
+				borderGray: '#272E3E',
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
 }

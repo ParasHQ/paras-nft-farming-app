c30bee61367e0bb81113e42efca28964ee1cdf28
Ahnaf Alfariza <ahnafalfariza@outlook.com>
2021-11-05 17:18:49 +0700

    add config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      parasGrey: "#171D2D",
+      blueButton: "#247DFE",
+      redButton: "#EB5757",
+      greenButton: "#21A79F",
+      borderGray: "#272E3E"
+    }
   },
   variants: {
     extend: {},

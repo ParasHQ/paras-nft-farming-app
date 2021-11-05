80303fa3a4e56b5262594b9b2596723d86fc9b1a
Ahnaf Alfariza <ahnafalfariza@outlook.com>
2021-11-05 22:21:32 +0700

    add: near setup and config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
 import 'tailwindcss/tailwind.css'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
+import near from 'services/near'
 
 function MyApp({ Component, pageProps }: AppProps) {
+	useEffect(() => {
+		near.init()
+	}, [])
+
 	return <Component {...pageProps} />
 }
 
---
+++ services/near.ts
@@ -0,0 +1,55 @@
+import { Near, keyStores, WalletConnection } from 'near-api-js'
+import getConfig from './config'
+
+interface IViewFunction {
+	contractName: string
+	methodName: string
+	args?: object
+}
+
+interface IFunctionCall extends IViewFunction {
+	gas?: string
+	amount?: string
+}
+
+class NearClass {
+	public near!: Near
+	public wallet!: WalletConnection
+
+	public init() {
+		const config = getConfig(process.env.APP_ENV || 'development')
+		const near = new Near({
+			keyStore: new keyStores.BrowserLocalStorageKeyStore(),
+			...config,
+		})
+		const wallet = new WalletConnection(near, 'paras-nft-farm')
+
+		this.near = near
+		this.wallet = wallet
+	}
+
+	public signIn() {
+		this.wallet.requestSignIn('example-contract.testnet', 'Paras NFT Farm')
+	}
+
+	public signOut() {
+		this.wallet.signOut()
+		window.location.replace(window.location.origin + window.location.pathname)
+	}
+
+	public nearFunctionCall({ methodName, args = {}, gas, amount, contractName }: IFunctionCall) {
+		return this.wallet.account().functionCall({
+			contractId: contractName,
+			methodName,
+			attachedDeposit: amount,
+			args,
+			gas,
+		})
+	}
+
+	public nearViewFunction({ methodName, args, contractName }: IViewFunction) {
+		return this.wallet.account().viewFunction(contractName, methodName, args)
+	}
+}
+
+export default new NearClass()

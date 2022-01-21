50c4cb6ebfd5d8be9bef112202ebb9a498c95f48
Ahnaf Alfariza <ahnafalfariza@outlook.com>
2022-01-21 13:16:08 +0700

    feat: target blank

--- pages/index.tsx
+++ pages/index.tsx
@@ -92,7 +92,8 @@ const Home: NextPage = () => {
 							<span className="opacity-80">Learn more about PARAS Staking </span>
 							<a
 								className="font-bold text-white opacity-100 border-b-2 border-transparent hover:border-gray-100"
-								href="https://paras.id/publication/now-live-earn-more-paras-by-paras-nft-staking-61e97465e26ebffd22443d4a "
+								href="https://paras.id/publication/now-live-earn-more-paras-by-paras-nft-staking-61e97465e26ebffd22443d4a"
+								target="_blank"
 							>
 								HERE
 							</a>
